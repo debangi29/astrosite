4576c40215d7fc7567398b873ab36345ff6ba219
Debangi Ghosh <117537653+debangi29@users.noreply.github.com>
2025-02-18 16:59:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/Astro_site',  // Replace with your GitHub repository name
   images: {
     domains: ['cloud.appwrite.io'], // Allow loading images from Appwrite
   },
+  trailingSlash: true, // Ensures correct path handling for routing
 }
 
 module.exports = nextConfig

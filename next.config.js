a0076e9b0485e5b7f1b0128a471b3b59e352f51a
debangi29 <debangi.ghosh.mat22@itbhu.ac.in>
2025-02-18 17:51:19 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/Astro_site',  // Replace with your GitHub repository name
+  basePath: '/astrosite',  // Replace with your GitHub repository name
   images: {
     domains: ['cloud.appwrite.io'], // Allow loading images from Appwrite
   },
